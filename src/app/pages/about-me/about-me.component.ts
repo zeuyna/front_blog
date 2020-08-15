0d82d82d7b62c938012bda78de8eeebc1647b28a
221geek <abdoulazizdione1997@gmail.com>
2020-08-15 17:00:57 +0000

    header

--- src/app/pages/about-me/about-me.component.ts
+++ src/app/pages/about-me/about-me.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-about-me',
   templateUrl: './about-me.component.html',
-  styleUrls: ['./about-me.component.scss']
+  styleUrls: ['./about-me.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class AboutMeComponent implements OnInit {
 
